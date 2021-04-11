fc76360694b4486ce1b2ec5c84688177be475c9a
Filipe Sá <62579882+sa-filipe@users.noreply.github.com>
2021-04-11 11:41:34 -0300

    Update mCode.js

--- mCode.js
+++ mCode.js
@@ -1,37 +1,58 @@
 // Barra inferior com informações
 (function() {
 
-    var div = document.createElement('div');
-    div.style.color = '#fafbfc';
-    div.style.width = '100%';
-    div.style.padding = '1px';
-    div.style.fontWeight = 'bold';
-    div.style.textAlign = 'center';
-    div.textContent = '🟣 TWITCH.TV 🟣⠀⠀"Twitch Claim Extension" is running on background!⠀⠀Check for updates⠀⠀https://github.com/sa-filipe/twitch-claim';
-    document.body.appendChild(div);
-    
+  var div = document.createElement('div');
+  div.style.color = '#fafbfc';
+  div.style.width = '100%';
+  div.style.padding = '1px';
+  div.style.fontWeight = 'bold';
+  div.id = 'marquee';
+  document.body.appendChild(div);
+
+  document.getElementById('marquee').animate([
+    {
+      transform: 'translateX(-50%)' 
+    },
+    {
+      transform: 'translateX(100%)'
+    }],
+    {
+      duration: 60000,
+      iterations: Infinity
+    });
+
 })();
 
-// Loop da coleta
+// Loop de coleta
 (async function() {
 
-    var i = 0;
-    var className = 'tw-button tw-button--success'; 
+  var count = 0;
+  var random = 1;
+  var className = 'tw-button tw-button--success';
     
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
+  function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+  }
+    
+  do {
+    var element = document.getElementsByClassName(className); 
+
+    // hide video
+
+    if (element.length > 0) { 
+      count++;
+      document.querySelector('div:nth-child(1) > div > div > div > div.tw-full-height.tw-relative.tw-z-above > div > div > div > button').click();
+      await sleep(1000);
+    } else {
+      if ( random <= 1) {
+        random++;
+        document.getElementById('marquee').textContent = 'Twitch Claim is running on background!⠀⠀🟣 TWITCH.TV 🟣⠀⠀https://github.com/sa-filipe/twitch-claim';
+      } else {
+        random-=1;
+        document.getElementById('marquee').textContent = 'Twitch Claim is running on background!⠀⠀🟣 COUNTER '+count+' 🟣⠀⠀https://github.com/sa-filipe/twitch-claim';
+      }
+      await sleep(5000);
     }
-    
-    do {
-        var element = document.getElementsByClassName(className); 
-        if (element.length > 0) { 
-            document.querySelector("div:nth-child(1) > div > div > div > div.tw-full-height.tw-relative.tw-z-above > div > div > div > button").click();
-            i++;
-            console.log('Claim!')
-            await sleep(1000);
-        } else {
-            await sleep(1000);
-        }
-    } while (i <= 9999999999);
+  } while (count <= 9999999999);
     
 })();
